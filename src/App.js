d18a1d3ba0a978e59eb4e3fc816f5d3901c05a53
Will Thompson <williamjamesthompson@gmail.com>
2020-06-04 11:29:59 -0500

    Created a 404

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import {Layout, Menu} from 'antd';
-import Products from './components/ProductList'
-import './App.css';
+import React from "react";
+import { Layout, Menu } from "antd";
+import Products from "./components/ProductList";
+import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Product from './pages/Product';
+import Product from "./pages/Product";
 
 const { Header, Content, Footer } = Layout;
 function App(props) {
   return (
     <div className="App">
       <Layout className="layout">
-          <Header id="header">         
-          </Header>
-          <Content style={{ padding: '1em'}}>
+        <Header id="header"></Header>
+        <Content style={{ padding: "1em" }}>
           <Router>
             <Switch>
-              <Route path="/product/:productId" component={Product}></Route>
+              <Route
+                path="/product/:productId"
+                exact
+                component={Product}
+              ></Route>
+
               <Route path="/" component={Products}></Route>
+              <Route component={Products}></Route>
             </Switch>
           </Router>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Berluti ©2020</Footer>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>Berluti ©2020</Footer>
       </Layout>
     </div>
   );
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -3,6 +3,8 @@ import React from "react";
 import "antd/dist/antd.css";
 import { PageHeader, Button } from "antd";
 import { ProductList } from "../config/Configs.js";
+import { BrowserRouter as Link } from "react-router-dom";
+
 import Form from "../components/Form";
 
 import { apply2DSpin } from "../Helpers/Spin";
@@ -29,68 +31,78 @@ class Product extends React.Component {
     const productId = this.props.match.params.productId;
     const product = ProductList[productId];
 
-    // Put player here
-    window
-      .threekitPlayer({
-        authToken: "fafe1cc5-0486-4b2f-8e0e-3a416f97f9d9",
-        display: "image",
-        el: document.getElementById("player"),
-        assetId: `${product.threekit}`,
-        stageId: `${product.stageId}`,
+    !product
+      ? console.log("no product")
+      : // Put player here
+        window
+          .threekitPlayer({
+            authToken: "fafe1cc5-0486-4b2f-8e0e-3a416f97f9d9",
+            display: "image",
+            el: document.getElementById("player"),
+            assetId: `${product.threekit}`,
+            stageId: `${product.stageId}`,
 
-        showConfigurator: false,
-        initialConfiguration: product.config,
-      })
-      .then(async (api) => {
-        window.player = api;
-        window.configurator = await api.getConfigurator();
+            showConfigurator: false,
+            initialConfiguration: product.config,
+          })
+          .then(async (api) => {
+            window.player = api;
+            window.configurator = await api.getConfigurator();
 
-        api.on(
-          window.player.scene.PHASES.RENDERED,
-          apply2DSpin({ attrName: "Rotation", direction: -1 })
-        );
-      });
+            api.on(
+              window.player.scene.PHASES.RENDERED,
+              apply2DSpin({ attrName: "Rotation", direction: -1 })
+            );
+          });
   }
   render() {
     const productId = this.props.match.params.productId;
     const product = ProductList[productId];
     return (
       <div id="s">
-        <PageHeader
-          style={{
-            border: "1px solid rgb(235, 237, 240)",
-          }}
-          onBack={() => window.location.assign("/")}
-          title={product.name}
-        />
-        <div id="threekit-container">
-          <div
-            id="player"
-            style={{
-              height: 400,
-              width: "96%",
-              marginLeft: "auto",
-              marginRight: "auto",
-              background: "#ffffff",
-            }}
-          ></div>
-        </div>
-        <Form config={product.config} />
-        {product.name == "Chevron" ?
-        <Button
-          onClick={() =>
-            window.open(
-              this.getImg(
-                window.configurator.getConfiguration().Rotation,
-                chevronMap
-              ),
-              "_blank"
-            )
-          }
-        >
-          Detailed View
-        </Button>
-        : <p></p> }
+        {!product ? (
+          <div>Invalid product URL. Please navigate <a href="/">home</a></div>
+        ) : (
+          <div>
+            <PageHeader
+              style={{
+                border: "1px solid rgb(235, 237, 240)",
+              }}
+              onBack={() => window.location.assign("/")}
+              title={product.name}
+            />
+            <div id="threekit-container">
+              <div
+                id="player"
+                style={{
+                  height: 400,
+                  width: "96%",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  background: "#ffffff",
+                }}
+              ></div>
+            </div>
+            <Form config={product.config} />
+            {product.name == "Chevron" ? (
+              <Button
+                onClick={() =>
+                  window.open(
+                    this.getImg(
+                      window.configurator.getConfiguration().Rotation,
+                      chevronMap
+                    ),
+                    "_blank"
+                  )
+                }
+              >
+                Detailed View
+              </Button>
+            ) : (
+              <p></p>
+            )}
+          </div>
+        )}
       </div>
     );
   }
